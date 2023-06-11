f25dbabc5610abf732174dccc5b8b87bab4192eb
Brenda Stockl <brendastockl94@gmail.com>
2023-06-11 19:59:06 -0300

    Commit del proyecto base por todo el equipo

---
+++ src/main/java/com/TpObjetos2/TpGrupo12/TpGrupo12Application.java
@@ -0,0 +1,13 @@
+package com.TpObjetos2.TpGrupo12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpGrupo12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpGrupo12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/TpObjetos2/TpGrupo12/TpGrupo12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TpObjetos2.TpGrupo12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpGrupo12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
